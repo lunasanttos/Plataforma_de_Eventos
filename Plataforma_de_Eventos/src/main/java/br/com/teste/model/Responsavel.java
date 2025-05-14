3fd6dd563970a554ddfe35c425cfad412cc29e3b
Luna Santos <estelafluna@gmail.com>
2025-05-13 21:18:02 -0300

    Add files via upload

---
+++ Plataforma_de_Eventos/src/main/java/br/com/teste/model/Responsavel.java
@@ -0,0 +1,28 @@
+package br.com.teste.model;
+
+public class Responsavel {
+    private int id_responsavel;
+    private String nome;
+    private String email;
+
+    public int getId_responsavel() {
+        return id_responsavel;
+    }
+    public void setId_responsavel(int id_responsavel) {
+        this.id_responsavel = id_responsavel;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String matricula) {
+        this.email = email;
+    }
+}
